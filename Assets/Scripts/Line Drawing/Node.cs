c1e7120fe8236c1e77e9bb134f9c64cf15c65ea6
sa287 <sa287@hw.ac.uk>
2023-03-28 01:00:22 +0100

    added pathfinding

---
+++ Assets/Scripts/Line Drawing/Node.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/*To create a node structure to use, we need to create a class for the Node. It holds a parent position, for a breadcrumb trail path to the goal node, and a Vector3Int position (the nodes current position).*/
+
+public class Node
+{
+    public Node Parent { get; set; }
+    public Vector3Int Position { get; set; }
+
+    public Node(Vector3Int position)
+    {
+        this.Position = position;
+    }
+}
